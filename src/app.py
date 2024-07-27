636ff54d6da50f26f27eaf9eda3948ebd78f72ba
Lsiniestro <164952161+Lsiniestro@users.noreply.github.com>
2024-07-26 21:35:30 -0300

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -3,21 +3,28 @@
 from pickle import load
 import streamlit as st
 
-model = load(open("../models/decision_tree_classifier_default_42.sav", "rb"))
+model = load(open(r"C:\Users\wipip\OneDrive\Documentos\GitHub\Despliegue_con_Streamlit-main\models\decision_tree_classifier_default_42.sav", "rb"))
 class_dict = {
-    "0": "Iris setosa",
-    "1": "Iris versicolor",
-    "2": "Iris virginica"
+    "0": "No Survived",
+    "1": "Survived"
 }
 
-st.title("Iris - Model prediction")
+st.title("Titanic Survivors prediction")
 
-val1 = st.slider("Petal width", min_value = 0.0, max_value = 4.0, step = 0.1)
-val2 = st.slider("Petal length", min_value = 0.0, max_value = 4.0, step = 0.1)
-val3 = st.slider("Sepal width", min_value = 0.0, max_value = 4.0, step = 0.1)
-val4 = st.slider("Sepal length", min_value = 0.0, max_value = 4.0, step = 0.1)
 
+# Entrada de características categóricas y numéricas
+pclass = st.selectbox("Passenger Class", options=[1, 2, 3])
+sex = st.selectbox("Sex of the Passenger", options=["Male", "Female"])
+FamMembers = st.slider("Number of Siblings/Spouses Aboard", min_value=0, max_value=10, step=1)
+fare = st.slider("Fare Paid", min_value=0.0, max_value=500.0, step=0.1)
+embarked = st.selectbox("Place of Embarkment", options=["C", "Q", "S"])
+
+# Convertir variables categóricas a valores numéricos
+sex_n = 1 if sex == "Male" else 0
+embarked_n = {"C": 0, "Q": 1, "S": 2}[embarked]
+
+# Botón de predicción
 if st.button("Predict"):
-    prediction = str(model.predict([[val1, val2, val3, val4]])[0])
+    prediction = str(model.predict([[pclass, sex_n, FamMembers, fare, embarked_n]])[0])
     pred_class = class_dict[prediction]
     st.write("Prediction:", pred_class)
